93c970c023f4c2e625f953cf8d934692e7e43bee
Chris O'Donnell <dev@codfish.io>
2021-01-06 16:18:17 -0500

    feat: add google analytics tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { pageview } from '../lib/gtag';
 import Layout from '../components/Layout';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../styles/app.css';
 
-const App = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>Kate Bromley</title>
-      <meta charSet="utf-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-      />
-      <meta name="robots" content="noindex, nofollow" />
-      <meta name="theme-color" content="#ED6D90" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
 
-      <meta
-        name="description"
-        content="Kate Bromley author of Talk Bookish to Me, a rom-com novel."
-      />
-    </Head>
+  useEffect(() => {
+    const handleRouteChange = url => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  </>
-);
+  return (
+    <>
+      <Head>
+        <title>Kate Bromley</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="theme-color" content="#ED6D90" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
+        <meta
+          name="description"
+          content="Kate Bromley author of Talk Bookish to Me, a rom-com novel."
+        />
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+};
 
 App.propTypes = {
   Component: PropTypes.elementType.isRequired,
---
+++ pages/_document.js
@@ -0,0 +1,38 @@
+/* eslint-disable react/no-danger */
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
