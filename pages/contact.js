6651dbf3ac378e22ec71c6b68c2cdd2bc84d2898
Chris O'Donnell <dev@codfish.io>
2022-07-24 23:48:22 -0400

    feat: remove contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,76 +1,19 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React from 'react';
 import Head from 'next/head';
 import { FaInstagram, FaTwitterSquare, FaAmazon, FaGoodreads } from 'react-icons/fa';
-import { fetchAPI } from '../lib/api';
 import PageHeader from '../components/PageHeader';
 import Section from '../components/Section';
 import SubscribeSection from '../components/SubscribeSection';
-import Button from '../components/Button';
 import Link from '../components/Link';
 
 function Contact() {
-  const [submitting, setSubmitting] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const [error, setError] = useState(false);
-  const nameRef = React.createRef();
-  const emailRef = React.createRef();
-  const messageRef = React.createRef();
-
-  const sendContact = async (event, body) => {
-    try {
-      await fetchAPI('/api/contact', { body });
-      setError(false);
-      setSubmitting(false);
-      setSuccess(true);
-      event.target.reset();
-    } catch (err) {
-      setSubmitting(false);
-      setSuccess(false);
-      setError(true);
-    }
-  };
-
-  const handleSubmit = async event => {
-    event.preventDefault();
-    // declaring these here cause if you try and grab them from within the recaptcha
-    // callback function, the ref's won't have their values...weird js scopes
-    const values = {
-      name: nameRef.current.value,
-      email: emailRef.current.value,
-      message: messageRef.current.value,
-    };
-
-    if (success) {
-      return;
-    }
-
-    setSubmitting(true);
-
-    window.grecaptcha.ready(() => {
-      window.grecaptcha
-        .execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY, { action: 'submit' })
-        .then(token => {
-          sendContact(event, { ...values, 'g-recaptcha-response': token });
-        })
-        .catch(() => {
-          setSubmitting(false);
-          setSuccess(false);
-          setError(true);
-        });
-    });
-  };
-
   return (
     <>
       <Head>
         <title>Contact Kate Bromley</title>
         <meta name="description" content="Contact Kate Bromley" />
         <link rel="canonical" href="https://www.katebromley.com/contact" />
-        <script
-          async
-          src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
-        />
       </Head>
 
       <PageHeader>Contact</PageHeader>
@@ -113,87 +56,6 @@ function Contact() {
             </a>
             .
           </p>
-
-          <p className="body1 mb-6">Send Kate a Message</p>
-
-          <form action="/api/contact" method="post" className="mb-20" onSubmit={handleSubmit}>
-            <label htmlFor="name" className="sr-only">
-              Full Name *
-            </label>
-            <input
-              type="text"
-              name="name"
-              placeholder="Full Name"
-              id="name"
-              ref={nameRef}
-              required
-              aria-required
-              className="input mb-6 rounded-sm"
-            />
-
-            <label htmlFor="email" className="sr-only">
-              Your Email *
-            </label>
-            <input
-              type="email"
-              name="email"
-              ref={emailRef}
-              placeholder="Your Email"
-              id="email"
-              required
-              aria-required
-              className="input mb-6 rounded-sm"
-            />
-
-            <label htmlFor="message" className="caption mb-2">
-              Message
-            </label>
-            <textarea
-              name="message"
-              placeholder="Type Your Message"
-              id="message"
-              ref={messageRef}
-              rows="5"
-              required
-              aria-required
-              className="textarea mb-2 rounded-sm"
-            />
-
-            <p className="caption mb-6 text-recaptcha">
-              This site is protected by reCAPTCHA and the Google{' '}
-              <a href="https://policies.google.com/privacy" target="_blank" rel="noreferrer">
-                Privacy Policy
-              </a>{' '}
-              and{' '}
-              <a href="https://policies.google.com/terms" target="_blank" rel="noreferrer">
-                Terms of Service
-              </a>{' '}
-              apply.
-            </p>
-
-            {success && (
-              <p className="body2 text-success-main">
-                Message was sent! Thanks for contacting Kate!
-              </p>
-            )}
-
-            {error && (
-              <p className="body2 text-error-main mb-6">
-                There was an issue sending the message. Please try again.
-              </p>
-            )}
-
-            {!success && (
-              <Button
-                type="submit"
-                primary
-                className="w-full sm:w-auto block sm:mx-auto"
-                disabled={submitting || success}
-              >
-                Send Message
-              </Button>
-            )}
-          </form>
 
           <div className="flex flex-col justify-center text-center">
             <h5 className="h5 text-primary-main mb-6 uppercase">Follow along on social media</h5>
